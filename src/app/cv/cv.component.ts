735120692980793260910df28c972a76149c186c
nicholasm185 <43334128+nicholasm185@users.noreply.github.com>
2020-03-19 19:23:24 +0700

    wow progress bar actually works

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -9,16 +9,36 @@ import {element} from 'protractor';
   styleUrls: ['./cv.component.css']
 })
 export class CvComponent implements OnInit {
+  data;
   sk1p;
   sk2p;
   sk3p;
+  // sk1style = {
+  //   width: this.sk1p + '%'
+  // };
+  // sk2style = {
+  //   width: this.sk2p
+  // };
+  // sk3style = {
+  //   width: this.sk3p
+  // };
 
   constructor(
     public loginService: LoginService,
   ) { }
 
   ngOnInit(): void {
-    // this.sk1p = this.loginService.user$.subscribe()
+    this.data = this.loginService.user$.subscribe(
+      data => {this.sk1p = (data.sk1p * 10 + '%');
+               this.sk2p = (data.sk2p * 10 + '%');
+               this.sk3p = (data.sk3p * 10 + '%'); }
+    );
+  }
+
+  buttonPress() {
+    console.log(this.sk1p);
+    console.log(this.sk2p);
+    console.log(this.sk3p);
   }
 
 }
